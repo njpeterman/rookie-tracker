c85a1060173f1e754e5e3c1e7da2cffeddb042b9
npeterm2 <npeterm2@illinois.edu>
2018-06-07 15:54:37 -0500

    Added ValueRanges for new service time row

--- src/rookie_tracker/MilbRostersUpdater.java
+++ src/rookie_tracker/MilbRostersUpdater.java
@@ -10,68 +10,84 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.SAXException;
 
+import com.google.api.client.util.Value;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
 public class MilbRostersUpdater {
 
 	private static String[] playerLinkRanges = {
 			"C2:G2", // team 1
-			"C8:G8", // team 2
-			"C14:G14", // ...
-			"C20:G20",
-			"C26:G26",
-			"C32:G32",
-			"C38:G38",
+			"C9:G9", // team 2
+			"C16:G16", // ...
+			"C23:G23",
+			"C30:G30",
+			"C37:G37",
 			"C44:G44",
-			"C50:G50",
-			"C56:G56",
-			"C62:G62",
-			"C68:G68", // team 12
+			"C51:G51",
+			"C58:G58",
+			"C65:G65",
+			"C72:G72",
+			"C79:G79", // team 12
 		};
 	
 	private static String[] playerAtBatsRanges = {
 			"C3:G3", // team 1
-			"C9:G9", // team 2
-			"C15:G15", // ...
-			"C21:G21",
-			"C27:G27",
-			"C33:G33",
-			"C39:G39",
+			"C10:G10", // team 2
+			"C17:G17", // ...
+			"C24:G24",
+			"C31:G31",
+			"C38:G38",
 			"C45:G45",
-			"C51:G51",
-			"C57:G57",
-			"C63:G63",
-			"C69:G69", // team 12
+			"C52:G52",
+			"C59:G59",
+			"C66:G66",
+			"C73:G73",
+			"C80:G80", // team 12
 	};
 	
 	private static String[] playerInningsPitchedRanges = {
 			"C4:G4", // team 1
-			"C10:G10", // team 2
-			"C16:G16", // ...
-			"C22:G22",
-			"C28:G28",
-			"C34:G34",
-			"C40:G40",
+			"C11:G11", // team 2
+			"C18:G18", // ...
+			"C25:G25",
+			"C32:G32",
+			"C39:G39",
 			"C46:G46",
-			"C52:G52",
-			"C58:G58",
-			"C64:G64",
-			"C70:G70", // team 12
+			"C53:G53",
+			"C60:G60",
+			"C67:G67",
+			"C74:G74",
+			"C81:G81", // team 12
+	};
+	
+	private static String[] playerServiceTimeRanges = {
+			"C5:G5", // team 1
+			"C12:G12", // team 2
+			"C19:G19", // ...
+			"C26:G26",
+			"C33:G33",
+			"C40:G40",
+			"C47:G47",
+			"C54:G54",
+			"C61:G61",
+			"C68:G68",
+			"C75:G75",
+			"C82:G82", // team 12
 	};
 	
 	private static String[] playerRookieStatusRanges = {
-			"C5:G5", // team 1
-			"C11:G11", // team 2
-			"C17:G17", // ...
-			"C23:G23",
-			"C29:G29",
-			"C35:G35",
+			"C6:G6", // team 1
+			"C13:G13", // team 2
+			"C20:G20", // ...
+			"C27:G27",
+			"C34:G34",
 			"C41:G41",
-			"C47:G47",
-			"C53:G53",
-			"C59:G59",
-			"C65:G65",
-			"C71:G71", // team 12
+			"C48:G48",
+			"C55:G55",
+			"C62:G62",
+			"C69:G69",
+			"C76:G76",
+			"C83:G83", // team 12
 	};
 	
 	public static void main(String[] args) throws IOException, GeneralSecurityException, SAXException, ParserConfigurationException {
@@ -92,6 +108,7 @@ public class MilbRostersUpdater {
 		// One ValueRange per team. Each ValueRange will contain the data for the team's players.
 		List<ValueRange> playerAtBatsData = new ArrayList<>();
 		List<ValueRange> playerInningsPitchedData = new ArrayList<>();
+		List<ValueRange> playerServiceTimeData = new ArrayList<>();
 		List<ValueRange> playerRookieStatusData = new ArrayList<>();
 		
 		for (int i = 0; i < response.size(); i++) { // For each team's list of player links
@@ -101,10 +118,12 @@ public class MilbRostersUpdater {
 			
 			ValueRange atBatsVR = new ValueRange();
 			ValueRange inningsPitchedVR = new ValueRange();
+			ValueRange serviceTimeVR = new ValueRange();
 			ValueRange rookieStatusVR = new ValueRange();
 			
 			List<Object> atBats = new ArrayList<>();
 			List<Object> inningsPitched = new ArrayList<>();
+			List<Object> serviceTime = new ArrayList<>();
 			List<Object> rookieStatus = new ArrayList<>();
 			
 			for (Object playerLink : playerLinks) {
@@ -112,10 +131,12 @@ public class MilbRostersUpdater {
 				if(playerStats != null) {
 					atBats.add(Integer.toString(playerStats.GetAtBats()));
 					inningsPitched.add(Double.toString(playerStats.GetInningsPitched()));
+					serviceTime.add(Integer.toString(playerStats.GetServiceTime()));
 					rookieStatus.add(Boolean.toString(playerStats.GetRookieStatus()).toUpperCase());
 				} else {
 					atBats.add("N/A");
 					inningsPitched.add("N/A");
+					serviceTime.add("N/A");
 					rookieStatus.add("NOT FOUND");
 				}
 				
@@ -123,19 +144,23 @@ public class MilbRostersUpdater {
 			
 			atBatsVR.setValues(new ArrayList<List<Object>>(Arrays.asList(atBats)));
 			inningsPitchedVR.setValues(new ArrayList<List<Object>>(Arrays.asList(inningsPitched)));
+			serviceTimeVR.setValues(new ArrayList<List<Object>>(Arrays.asList(serviceTime)));
 			rookieStatusVR.setValues(new ArrayList<List<Object>>(Arrays.asList(rookieStatus)));
 			
 			atBatsVR.setRange(playerAtBatsRanges[i]);
 			inningsPitchedVR.setRange(playerInningsPitchedRanges[i]);
+			serviceTimeVR.setRange(playerServiceTimeRanges[i]);
 			rookieStatusVR.setRange(playerRookieStatusRanges[i]);
 			
 			playerAtBatsData.add(atBatsVR);
 			playerInningsPitchedData.add(inningsPitchedVR);
+			playerServiceTimeData.add(serviceTimeVR);
 			playerRookieStatusData.add(rookieStatusVR);
 		}
 	
 		wrapper.UpdateBatchValues(spreadsheetId, apiKey, clientSecretFilePath, playerAtBatsData);
 		wrapper.UpdateBatchValues(spreadsheetId, apiKey, clientSecretFilePath, playerInningsPitchedData);
+		wrapper.UpdateBatchValues(spreadsheetId, apiKey, clientSecretFilePath, playerServiceTimeData);
 		wrapper.UpdateBatchValues(spreadsheetId, apiKey, clientSecretFilePath, playerRookieStatusData);		
 	}
 
